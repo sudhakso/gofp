a1db4cff66426d209f3e7a27a063b4773d2547b3
sudhakso <sonu.sudhakaran@hpe.com>
2023-12-22 14:11:52 +0530

    ocp

--- README.md
+++ README.md
@@ -16,8 +16,21 @@ Few principles to take into consideration while doing code designing (SOLID)
    * Journal type, with Add, Remove, String and SaveToFile() implementation
 
    It is also called as *SoC* : Separation of concerns.
-   
+
 2. OCP - Open Closed Principle
+
+    :heavy_check_mark: A type is closed for *any* modifications to suit the needs of future requirements but is open for *extensions*.
+
+    An easy similarity would be to compare it to database design. 
+    * You do not change the existing fields in the DB table
+    * You add new tables, and have new features exposed through application code.
+
+    Another concrete example is,
+    * *Product* type, with *Size* & *Color* as its attributes
+    * Extending the Product type to support *filtering* capabilities, such that, you can filter the Products by *Size*, *Colo* or both.
+
+    When compared to
+    * Implement *Filter* type, to support filtering by *Size*, *Color*, both *size* & *color*, and in future keep changing the *Filter* type to add new features.
 
 3. LSP - Liskov Substitution Principle
 
--- cmd/ocp/main.go
+++ cmd/ocp/main.go
@@ -2,6 +2,113 @@ package main
 
 import "fmt"
 
+type Color int
+
+const (
+	green Color = iota
+	red
+	blue
+)
+
+type Size int
+
+const (
+	small Size = iota
+	big
+)
+
+// concrete type
+type Product struct {
+	name  string
+	color Color
+	size  Size
+}
+
+// concrete receiver type for filtering by features - anti-pattern as per OCP
+// We have to change Filter type each time to extend the filtering capabilities.
+type Filter struct {
+}
+
+func (f *Filter) filterByColor(p []Product, color Color) []*Product {
+	fmt.Printf("Filtering by color (Old)\n")
+	result := make([]*Product, 0)
+	for i, v := range p {
+		if v.color == color {
+			result = append(result, &p[i])
+		}
+	}
+	return result
+}
+
+func (f *Filter) filterBySize(p []Product, size Size) []*Product {
+	fmt.Printf("Filtering by size (Old)\n")
+	result := make([]*Product, 0)
+	for i, v := range p {
+		if v.size == size {
+			result = append(result, &p[i])
+		}
+	}
+	return result
+}
+
+// OCP compliant way
+
+type FilterSpec interface {
+	IsSatisfied(*Product) bool
+}
+
+type ColorFilter struct {
+	color Color
+}
+
+func (cf *ColorFilter) IsSatisfied(prod *Product) bool {
+	return cf.color == prod.color
+}
+
+type SizeFilter struct {
+	size Size
+}
+
+func (sf *SizeFilter) IsSatisfied(product *Product) bool {
+	return sf.size == product.size
+}
+
 func main() {
 	fmt.Println("OCP Main...")
+
+	apple := Product{"apple", green, small}
+	tree := Product{"tree", green, big}
+	house := Product{"house", red, big}
+
+	products := []Product{apple, tree, house}
+
+	// Calling filters - anti-pattern
+	f := Filter{}
+	gp := f.filterByColor(products, green)
+	for _, v := range gp {
+		fmt.Printf(" - %s is green\n", v.name)
+	}
+
+	sp := f.filterBySize(products, small)
+	for _, v := range sp {
+		fmt.Printf(" - %s is small\n", v.name)
+	}
+
+	// Doing the OCP compliant way
+	cf := ColorFilter{green}
+	fmt.Println("Filtering by Color (New)")
+	for _, v := range products {
+		if cf.IsSatisfied(&v) {
+			fmt.Printf(" - %s is green\n", v.name)
+		}
+	}
+
+	// Doing the OCP compliant way
+	sf := SizeFilter{small}
+	fmt.Println("Filtering by Size (New)")
+	for _, v := range products {
+		if sf.IsSatisfied(&v) {
+			fmt.Printf(" - %s is small\n", v.name)
+		}
+	}
 }
